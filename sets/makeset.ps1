b402b804bbf88a059f083b1227e3e318c5f422f8
Tuatarian <betterthangeowizard@gmail.com>
2022-04-03 22:20:55 -0700

    even more bugfixing

--- sets/makeset.ps1
+++ sets/makeset.ps1
@@ -1,6 +1,6 @@
 $urls = "https://lh3.googleusercontent.com/iecdsx8DH5VOWqqqVHB90cvu33yRxPet-DudjK3oySM3kOyvG30cZtYnDPBfxZphXg=s1200", "https://img.apmcdn.org/b0bac40848aac153d41caf45e2945d9b8e574b42/uncropped/6702c3-20150323-washington.jpg", "https://zantana.net/static/81f748bfb715775c0a798496b37e9ff7/38f6f/suleiman_i_sultan_of_the_ottoman_empire.jpg"
 $outs = "(c) Antonio Salazar de Oliveira.png", "(a) Washington Crossing the Delaware.png", "(b) Suleiman's Onion Hat.png"
-$setname = "Washington, Suleiman, Salazar(4,3,2022)"
+$setname = "Washington, Suleiman, Salazar (4,3,2022)"
 
 rm -Recurse $setname
 rm -Recurse "$setname.zip"
