59840cd6b8637b49093bbed1c3f589117eae32ab
Tuatarian <betterthangeowizard@gmail.com>
2022-04-10 20:19:37 -0700

    changed set download mechanism, avoiding image compression

--- sets/makeset.ps1
+++ sets/makeset.ps1
@@ -1,13 +1,19 @@
-$urls = "https://lh3.googleusercontent.com/iecdsx8DH5VOWqqqVHB90cvu33yRxPet-DudjK3oySM3kOyvG30cZtYnDPBfxZphXg=s1200", "https://img.apmcdn.org/b0bac40848aac153d41caf45e2945d9b8e574b42/uncropped/6702c3-20150323-washington.jpg", "https://zantana.net/static/81f748bfb715775c0a798496b37e9ff7/38f6f/suleiman_i_sultan_of_the_ottoman_empire.jpg", "https://ihsvoice.com/wp-content/uploads/2019/05/qb.jpg"
-$outs = "(c) Antonio Salazar de Oliveira.png", "(a) Washington Crossing the Delaware.png", "(b) Suleiman's Onion Hat.png", "(d) My father was a historian"
+$urls = "https://lh3.googleusercontent.com/iecdsx8DH5VOWqqqVHB90cvu33yRxPet-DudjK3oySM3kOyvG30cZtYnDPBfxZphXg=s1200", "https://img.apmcdn.org/b0bac40848aac153d41caf45e2945d9b8e574b42/uncropped/6702c3-20150323-washington.jpg", "https://zantana.net/static/81f748bfb715775c0a798496b37e9ff7/38f6f/suleiman_i_sultan_of_the_ottoman_empire.jpg"
+$outs = "(b) Antonio Salazar de Oliveira.png", "(a) Washington Crossing the Delaware.png", "(c) Suleiman's Onion Hat.png"
 $setname = "Washington, Suleiman, Salazar (04,03,2022)"
 
 rm -Recurse $setname
 rm -Recurse "$setname.zip"
 
-rm "../fname"
-ft > ../fname.txt
-Set-Content "../fname.txt" $setname
+rm "./files.txt"
+rm "./fname.txt"
+ft > "./fname.txt"
+ft > "./files.txt"
+Set-Content "./files.txt" "$setname&!&"
+foreach ($i in 0..($urls.Count - 1)) {
+	Add-Content "./files.txt" "$($urls[$i])&!&"
+	Add-Content "./fname.txt" "$($outs[$i])&!&"
+}
 
 mkdir $setname
 foreach ($i in 0..($urls.Count-1)) {
