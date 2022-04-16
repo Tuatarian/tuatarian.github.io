4919eb5c883b80c9503afd9e9fa38b00dd2c57e5
Tuatarian <betterthangeowizard@gmail.com>
2022-04-15 18:41:44 -0700

    final and probably working version!

--- sets/makeset.ps1
+++ sets/makeset.ps1
@@ -1,24 +1,20 @@
 $urls = "https://lh3.googleusercontent.com/iecdsx8DH5VOWqqqVHB90cvu33yRxPet-DudjK3oySM3kOyvG30cZtYnDPBfxZphXg=s1200", "https://img.apmcdn.org/b0bac40848aac153d41caf45e2945d9b8e574b42/uncropped/6702c3-20150323-washington.jpg", "https://zantana.net/static/81f748bfb715775c0a798496b37e9ff7/38f6f/suleiman_i_sultan_of_the_ottoman_empire.jpg"
-$outs = "(b) Antonio Salazar de Oliveira.png", "(a) Washington Crossing the Delaware.png", "(c) Suleiman's Onion Hat.png"
+$outs = "(b) Antonio Salazar de Oliveira.jpg", "(a) Washington Crossing the Delaware.jpg", "(c) Suleiman's Onion Hat.jpg"
 $setname = "Washington, Suleiman, Salazar (04,03,2022)"
 
-rm -Recurse $setname
+rm -Recurse "fname.txt"
 rm -Recurse "$setname.zip"
 
-rm "./files.txt"
-rm "./fnames.txt"
-ft > "./fnames.txt"
-ft > "./files.txt"
-Set-Content "./files.txt" "$setname"
-foreach ($i in 0..($urls.Count - 1)) {
-	Add-Content "./files.txt" "$($urls[$i])"
-	Add-Content "./fnames.txt" "$($outs[$i])"
-}
+ft > "fname.txt"
+Set-Content "fname.txt" $setname
+
 
 mkdir $setname
 foreach ($i in 0..($urls.Count-1)) {
 	Invoke-WebRequest -Uri $urls[$i] -OutFile $outs[$i]
-	mv $outs[$i] $setname
+	convert "$($outs[$i])" (("$($outs[$i])" -replace ".{3}$") + "png")
+	mv (("$($outs[$i])" -replace ".{3}$") + "png") $setname
+	rm $outs[$i]
 }
 cp imdat.txt $setname
 
