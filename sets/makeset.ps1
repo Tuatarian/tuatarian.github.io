36878ce6682c7b6594052bf464c8aab50af77b06
Tuatarian <betterthangeowizard@gmail.com>
2022-04-15 18:54:15 -0700

    formatting change

--- sets/makeset.ps1
+++ sets/makeset.ps1
@@ -1,6 +1,6 @@
 $urls = "https://lh3.googleusercontent.com/iecdsx8DH5VOWqqqVHB90cvu33yRxPet-DudjK3oySM3kOyvG30cZtYnDPBfxZphXg=s1200", "https://img.apmcdn.org/b0bac40848aac153d41caf45e2945d9b8e574b42/uncropped/6702c3-20150323-washington.jpg", "https://zantana.net/static/81f748bfb715775c0a798496b37e9ff7/38f6f/suleiman_i_sultan_of_the_ottoman_empire.jpg"
 $outs = "(b) Antonio Salazar de Oliveira.jpg", "(a) Washington Crossing the Delaware.jpg", "(c) Suleiman's Onion Hat.jpg"
-$setname = "Washington, Suleiman, Salazar (04,03,2022)"
+$setname = "(2022, 04, 15) Washington, Suleiman, Salazar"
 
 rm -Recurse "fname.txt"
 rm -Recurse "$setname.zip"
